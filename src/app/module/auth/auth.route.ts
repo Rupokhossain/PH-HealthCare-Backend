974b8e49ddca3297296954a57ce6cefbc23d9c10
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-08-10 16:26:46 +0600

    implementing zod validatoin

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,16 +1,51 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { Role } from "../../../generated/prisma/enums";
 import { auth } from "../../middleware/checkAuth";
 import { AuthController } from "./auth.controller";
+import { LoginvalidationSchema, PatientRegistration } from "./auth.validation";
+import { validateRequest } from "../../middleware/validateRequest";
 
 const router = Router();
 
-router.post("/register", AuthController.registerPatient);
-router.post("/login", AuthController.loginUser);
+router.post(
+  "/register",
+  //   (req: Request, res: Response, next: NextFunction) => {
+  // 	try {
+  // 		// const payload = req.body ? req.body : {}
+  // 		const payload = req.body ?? {}
+
+  // 		const result = PatientRegistration.safeParse(payload);
+
+  // 		if(!result.success) {
+  // 			console.log(result.error);
+  // 			console.log(result.error.issues);
+
+  // 			throw new Error(result.error.issues[0].message)
+  // 		}
+
+  // 		req.body = result.data
+
+  // 		next()
+
+  // 	} catch (error) {
+  // 		next(error)
+  // 	}
+  //   },
+
+  validateRequest(PatientRegistration),
+
+  AuthController.registerPatient,
+);
+
+router.post(
+  "/login",
+  validateRequest(LoginvalidationSchema),
+  AuthController.loginUser,
+);
 router.get(
-	"/me",
-	auth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN),
-	AuthController.getMe,
+  "/me",
+  auth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN),
+  AuthController.getMe,
 );
 router.post("/refresh-token", AuthController.refreshToken);
 router.post("/google", AuthController.googleLogin);
---
+++ src/app/module/auth/auth.validation.ts
@@ -0,0 +1,38 @@
+import z from "zod";
+
+export const PatientRegistration = z.object({
+  name: z.string(),
+  email: z.email(),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long.")
+    .max(32, "Password cannot exceed 32 characters.")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter.")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter.")
+    .regex(/[0-9]/, "Password must contain at least one number.")
+    .regex(
+      /[^A-Za-z0-9]/,
+      "Password must contain at least one special character.",
+    ),
+  patient: z
+    .object({
+      contactNumber: z.string().optional(),
+      age: z.number(),
+    })
+    .optional(),
+});
+
+export const LoginvalidationSchema = z.object({
+  email: z.email(),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long.")
+    .max(32, "Password cannot exceed 32 characters.")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter.")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter.")
+    .regex(/[0-9]/, "Password must contain at least one number.")
+    .regex(
+      /[^A-Za-z0-9]/,
+      "Password must contain at least one special character.",
+    ),
+});
